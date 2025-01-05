f468de2f376a4d5adb49c874f17ffa6c3f3a36b8
Devp-sriram <sriramraman100@gmail.com>
2025-01-05 19:42:37 +0530

    backend configured

--- config/global.js
+++ config/global.js
@@ -1,4 +1,6 @@
-const API_URL = "https://auth-be-ks25.onrender.com"
+import dotenv from 'dotenv'
+dotenv.config();
+const API_URL = process.env.BACKEND_URL
 // const API_URL = "http://localhost:4000"
 
 export default API_URL
