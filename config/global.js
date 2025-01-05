4bdd509e6038fe5d6a9e73656163c9b449191b30
Devp-sriram <sriramraman100@gmail.com>
2025-01-05 19:58:58 +0530

    env configured

--- config/global.js
+++ config/global.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config();
-const API_URL = process.env.BACKEND_URL
+const API_URL = process.env.VITE_BACKEND_URL
 // const API_URL = "http://localhost:4000"
 
 export default API_URL
